3b124c7e689f836db5ff1c7aeec9478838bbcee0
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-04-24 18:17:27 -0700

    Lab 3 - todolist

--- webpack.config.js
+++ webpack.config.js
@@ -4,49 +4,50 @@ const htmlWebpackPlugin = require("html-webpack-plugin");
 const copyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-      index: './src/js/index.js',
-    },
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: '[name].bundle.js',
-      assetModuleFilename: "images/[name][ext]",
-      clean: true,
-    },
-    target: 'web',
-    devServer: { 
-      static: "./dist"
-    }, 
-    devtool: 'source-map', 
-    module: {
-      rules: [	
-        { 
-          test: /\.js$/i,
-          exclude: /(node_modules)/,
-          use: { 
-            loader: 'babel-loader', 
-            options: {
-            presets: ['@babel/preset-env']
-          }}
-        }
-      ],
-    },
-    plugins: [
-      new htmlWebpackPlugin({
-        template: path.resolve(__dirname, "./src/index.html"),
-        chunks: ["index"],
-        inject: "body",
-        filename: "index.html",
-      }),
-      new copyPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, "src/css"),
-            to: path.resolve(__dirname, "dist/css"),
-          },
-        ],
-      }),
-    ],
+	mode: 'development',
+	entry: {
+		index: './src/js/index.js',
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: '[name].bundle.js',
+		assetModuleFilename: "images/[name][ext]",
+		clean: true,
+	},
+	target: 'web',
+	devServer: {
+		static: "./src"
+
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/i,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		],
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: path.resolve(__dirname, "./src/index.html"),
+			chunks: ["index"],
+			inject: "body",
+			filename: "index.html",
+		}),
+		new copyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, "src/css"),
+					to: path.resolve(__dirname, "dist/css"),
+				},
+			],
+		}),
+	],
 }
-  
